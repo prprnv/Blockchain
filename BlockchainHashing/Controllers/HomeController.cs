30e89c377dd4a74b8cad4e017bd5864094cb69d4
Pranav Kumar <prnvkmr954@gmail.com>
2018-01-09 11:57:01 +0530

    Added

---
+++ BlockchainHashing/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Mvc;
+using BlockchainHashing.ViewModels;
+using BlockchainHashing.Models;
+using System.Linq;
+
+namespace BlockchainHashing.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+        //[HttpPost]
+        //public ActionResult Index(string value)
+        //{
+        //    value = HashData.GetHashString(value);
+        //    return Json(value, JsonRequestBehavior.AllowGet);
+
+        //}
+
+        [HttpPost]
+        public ActionResult Index(BlockchainViewmodel data)
+        {
+            AllDummyTablesEntities db = new AllDummyTablesEntities();
+            var getdata = (from aa in db.Blockchains  select aa).SingleOrDefault();
+            if (getdata == null)
+            {
+                Blockchain addData = new Blockchain()
+                {
+                    Data=data.Data,
+                    Hash= HashData.GetHashString(data.Hash),
+                    PatientId=1
+                };
+                db.Blockchains.Add(addData);
+                db.SaveChanges();
+            }
+            else
+            {
+
+            }
+            return View();
+        }
+    }
+}
---
+++ BlockchainHashing/ViewModels/BlockchainViewmodel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlockchainHashing.ViewModels
+{
+	public class BlockchainViewmodel
+	{
+        public int? BlockId { get; set; }
+        public Nullable<int> PatientId { get; set; }
+        public string Nonce { get; set; }
+        public string Data { get; set; }
+        public string Hash { get; set; }
+        public string PreviousHash1 { get; set; }
+    }
+}
